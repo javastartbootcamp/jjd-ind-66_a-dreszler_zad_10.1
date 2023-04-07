be513360bb223220941eb9eacbaac25379e63e91
Aleksander Dreszler <olek.d93@gmail.com>
2023-04-07 17:43:05 +0200

    Wykonano zadanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,8 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Library library = new Library();
+        library.readBooks();
+        library.viewBooks();
     }
 }
