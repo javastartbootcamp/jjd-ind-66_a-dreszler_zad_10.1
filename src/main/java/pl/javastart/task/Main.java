6cabbd2e3b17f5c7a3dd74421b232175c49e93f2
Aleksander Dreszler <olek.d93@gmail.com>
2023-04-07 17:37:01 +0200

    Revert "Wykonano zadanie"
    This reverts commit d614a749133202db736c2de4cd2b84061c0cb5de.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,8 +3,6 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-        Library library = new Library();
-        library.readBooks();
-        library.viewBooks();
+
     }
 }
